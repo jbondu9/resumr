5783a6b29d342e593c66386b4b018d419a2f86f0
Jimmy Bondu <jimmy.bondu@zoho.com>
2024-02-01 23:59:00 +0100

    style: style the markers

--- src/components/MarkerList.component.tsx
+++ src/components/MarkerList.component.tsx
@@ -1,14 +1,23 @@
+import {
+  faBriefcase,
+  faGraduationCap,
+  faHashtag,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { DivIcon, divIcon } from "leaflet";
 import { ReactElement } from "react";
+import { renderToString } from "react-dom/server";
 import { Marker } from "react-leaflet";
 
+import { Category } from "../enums/Category.enum";
+import { CustomIcon } from "../types/CustomIcon.type";
 import { MarkerElement } from "../types/MarkerElement.type";
 
 export default function MarkerList({
   markers,
-  onClick,
 }: {
   markers: MarkerElement[];
-  onClick: (marker: MarkerElement) => void;
 }): ReactElement {
   return (
     <>
@@ -16,13 +25,50 @@ export default function MarkerList({
         <Marker
           key={marker.id}
           position={marker.position}
-          eventHandlers={{
-            click: () => {
-              onClick(marker);
-            },
-          }}
+          icon={customIcon(marker.category)}
         ></Marker>
       ))}
     </>
   );
 }
+
+const customIcon = (category: Category): DivIcon => {
+  const { icon, bgColor } = getIcon(category);
+
+  return divIcon({
+    html: renderToString(
+      <div
+        className={`${bgColor} flex size-6 -translate-x-1 -translate-y-1 items-center justify-center rounded-full text-white shadow-md outline outline-2 outline-white`}
+      >
+        {icon}
+      </div>,
+    ),
+  });
+};
+
+// This function must be present for Tailwind to interpret the background color correctly
+const getIcon = (category: Category): CustomIcon => {
+  switch (category) {
+    case Category.ACTIVITY: {
+      return {
+        icon: <FontAwesomeIcon icon={faHashtag} className="size-4" />,
+        bgColor: "bg-green-400",
+      };
+    }
+    case Category.EDUCATION: {
+      return {
+        icon: <FontAwesomeIcon icon={faGraduationCap} className="size-4" />,
+        bgColor: "bg-blue-400",
+      };
+    }
+    case Category.WORK: {
+      return {
+        icon: <FontAwesomeIcon icon={faBriefcase} className="size-4" />,
+        bgColor: "bg-orange-400",
+      };
+    }
+    default: {
+      throw new Error("CustomIcon Error: Unrecognized category");
+    }
+  }
+};
---
+++ src/types/CustomIcon.type.ts
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+
+export type CustomIcon = {
+  icon: ReactElement;
+  bgColor: string;
+};
